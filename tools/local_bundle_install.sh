e14314a90daa63c5952c863aceff842d99b0bdbe
Alex Birch <alexander.birch@billforward.net>
2015-07-02 11:18:11 +0100

    Add awesome directory-respecting local gem bundler & installer

---
+++ tools/local_bundle_install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# cd to parent dir of this script
+pushd $(cd -P -- "$(dirname -- "$0")" && pwd -P)/..
+# upon any kind of termination, return to our original directory
+trap "popd" EXIT SIGHUP SIGINT SIGTERM
+
+bundle
+gem build bill_forward.gemspec
+gem install bill_forward
