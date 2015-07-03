d05ef47ad59488379d9e49619775a4df747a1a2c
Alex Birch <alexander.birch@billforward.net>
2015-07-03 11:51:54 +0100

    Update credit note expectations

--- spec/functional/bad_citizen/credit_note_spec.rb
+++ spec/functional/bad_citizen/credit_note_spec.rb
@@ -11,24 +11,25 @@ describe BillForward::CreditNote do
 				before :all do
 					@created_account = BillForward::Account.create
 
-					payment_method = BillForward::PaymentMethod.new({
-						'accountID' => @created_account.id,
-						'name' => 'Credit Notes',
-						'description' => 'Pay using credit',
-						# engines will link this to an invoice once paid, for the sake of refunds
-						'linkID' => '',
-						'gateway' => 'credit_note',
-						'userEditable' => 0,
-						'priority' => 100,
-						'reusable' => 1
-						})
-					created_payment_method = BillForward::PaymentMethod::create(payment_method)
+					# account implicitly has these nowadays
+					# payment_method = BillForward::PaymentMethod.new({
+					# 	'accountID' => @created_account.id,
+					# 	'name' => 'Credit Notes',
+					# 	'description' => 'Pay using credit',
+					# 	# engines will link this to an invoice once paid, for the sake of refunds
+					# 	'linkID' => '',
+					# 	'gateway' => 'credit_note',
+					# 	'userEditable' => 0,
+					# 	'priority' => 100,
+					# 	'reusable' => 1
+					# 	})
+					# created_payment_method = BillForward::PaymentMethod::create(payment_method)
 				end
 				subject (:account) { @created_account }
 				it 'issues credit' do
 					credit_note = BillForward::CreditNote.new({
 						"accountID" => account.id,
-					    "nominalValue" => 15,
+					    "value" => 15,
 					    "currency" => "USD"
 						})
 					created_credit_note = BillForward::CreditNote.create(credit_note)
