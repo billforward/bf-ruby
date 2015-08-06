ec7c79df104769952a064b74b29f26857c30b112
Alex Birch <alexander.birch@billforward.net>
2015-08-06 11:24:20 +0100

    Add get_by_link_id

--- lib/bill_forward/entities/payment_method.rb
+++ lib/bill_forward/entities/payment_method.rb
@@ -1,5 +1,17 @@
 module BillForward
 	class PaymentMethod < MutableEntity
   		@resource_path = BillForward::ResourcePath.new("payment-methods", "paymentMethod")
+
+  		class << self
+  			def get_by_link_id(id, query_params = {}, custom_client = nil)
+		        raise ArgumentError.new("id cannot be nil") if id.nil?
+
+		        endpoint = sprintf('link-id/%s',
+		          ERB::Util.url_encode(id)
+		          )
+
+		        self.request_first('get', endpoint, query_params, custom_client)
+		    end
+  		end
 	end
 end
