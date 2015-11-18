96acc08548e46f0a3bfe9df2fcf882e948635c67
Alex Birch <alexander.birch@billforward.net>
2015-11-18 20:27:21 +0000

    apparently initialization is required to access self attributes. let's kill it so we can do get_all

--- lib/bill_forward/entities/amendments/amendment.rb
+++ lib/bill_forward/entities/amendments/amendment.rb
@@ -2,10 +2,10 @@ module BillForward
   class Amendment < InsertableEntity
   	@resource_path = BillForward::ResourcePath.new("amendments", "amendment")
 
-    def initialize(*args)
-      raise AbstractInstantiateError.new('This abstract class cannot be instantiated!') if self.class == Amendment
-      super
-    end
+    # def initialize(*args)
+    #   raise AbstractInstantiateError.new('This abstract class cannot be instantiated!') if self.class == Amendment
+    #   super
+    # end
 
   end
 end
