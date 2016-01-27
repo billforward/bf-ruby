8a2acbcaa248b824e0e786e97a574e83eda07ac5
Alex Birch <alexander.birch@billforward.net>
2016-01-27 12:48:26 +0000

    bump version (hello 2016!)

--- lib/bill_forward/version.rb
+++ lib/bill_forward/version.rb
@@ -1,4 +1,4 @@
 module BillForward
   # in an rspec run, the gemspec and bill_forward.rb loader will both visit this
-  VERSION = "1.2015.321.1" unless const_defined?(:VERSION)
+  VERSION = "1.2016.26" unless const_defined?(:VERSION)
 end
