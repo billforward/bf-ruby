f6f9963db5e6db6ce74d5339f4a2f347e70686ef
Alex Birch <alexander.birch@billforward.net>
2016-01-27 12:37:39 +0000

    Credit functions

--- lib/bill_forward/entities/credit_note.rb
+++ lib/bill_forward/entities/credit_note.rb
@@ -8,6 +8,42 @@ module BillForward
 			 'at the time of writing, no API endpoint exists to support it.'+
 			 'The entity can be GETted by ID only.'
 	  	end
+	  	
+		def get_remaining_credit_on_account(id, query_params = {}, custom_client = nil)
+			credit_notes = get_by_account_id(id, query_params, custom_client)
+
+			self.count_remaining_credit credit_notes
+		end
+	  	
+		def get_remaining_credit_on_subscription(id, query_params = {}, custom_client = nil)
+			credit_notes = get_by_subscription_id(id, query_params, custom_client)
+
+			self.count_remaining_credit credit_notes
+		end
+
+		def get_by_account_id(id, query_params = {}, custom_client = nil)
+			raise ArgumentError.new("id cannot be nil") if id.nil?
+
+			endpoint = sprintf('account/%s',
+				ERB::Util.url_encode(id)
+			)
+
+			self.request_many('get', endpoint, query_params, custom_client)
+		end
+	  	
+		def get_by_subscription_id(id, query_params = {}, custom_client = nil)
+			raise ArgumentError.new("id cannot be nil") if id.nil?
+
+			endpoint = sprintf('subscription/%s',
+				ERB::Util.url_encode(id)
+			)
+
+			self.request_many('get', endpoint, query_params, custom_client)
+		end
+	protected
+		def count_remaining_credit(credit_notes)
+			credit_notes.map(&:remainingValue).inject(0, :+)
+		end
 	end
   end
 end
