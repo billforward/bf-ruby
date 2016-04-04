e220754b68f013cb7640ea61c284c50561b83c22
Łukasz Kwasek <pamput@gmail.com>
2016-04-04 15:05:12 +0100

    add credit to invoice add full support for refunds

--- lib/bill_forward/entities/invoice.rb
+++ lib/bill_forward/entities/invoice.rb
@@ -63,6 +63,16 @@ module BillForward
 
         self.request_first('post', endpoint, request_entity, nil, custom_client)
       end
+
+      def get_charges(id, query_params = {}, custom_client = nil)
+        raise ArgumentError.new("id cannot be nil") if id.nil?
+
+        endpoint = sprintf('%s/charges',
+                           ERB::Util.url_encode(id)
+        )
+
+        self.request_many('get', endpoint, query_params, custom_client)
+      end
     end
 
   protected
